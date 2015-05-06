31ae0d442a4fd567d759d25a63531bd8da4422cf
patm2013 <patm2013@gmail.com>
2015-05-06 11:26:18 -0500

    Tabletop Setup

---
+++ js/ttCustom.js
@@ -0,0 +1,7 @@
+function init() {
+	Tabletop.init( { key: '1U90V7_jgHs4gOo-P2DjhaPXZRCTMllQhm-mxMEc44rQ',
+	               callback: function(data, tabletop) { console.log(data) },
+	               simpleSheet: true } )
+}
+
+init();
