efda84e491ff48ddd9fc2707b43c0431ecb47e72
patm2013 <patm2013@gmail.com>
2015-05-06 14:21:25 -0500

    ttClasses

--- js/ttCustom.js
+++ js/ttCustom.js
@@ -1,7 +1,17 @@
-function init() {
-	Tabletop.init( { key: '1U90V7_jgHs4gOo-P2DjhaPXZRCTMllQhm-mxMEc44rQ',
-	               callback: function(data, tabletop) { console.log(data) },
-	               simpleSheet: true } )
-}
+$(document).ready(function() {
 
-init();
+	function init() {
+		Tabletop.init( { key: '1U90V7_jgHs4gOo-P2DjhaPXZRCTMllQhm-mxMEc44rQ',
+           callback: function(data, tabletop) { addToWB(data) },
+           simpleSheet: true } )
+	}
+	init();
+
+	var addToWB = function(ttData){
+		var templateData = {
+                ttClasses: ttData
+            };
+		template = _.template($("#class-template").html());
+            $("#gClasses").html(template(templateData));
+    };
+});
